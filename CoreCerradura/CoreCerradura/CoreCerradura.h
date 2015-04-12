3a9293ca62ae5d2f8400252f5e001a4922596925
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-04-12 10:57:41 -0500

    created model

--- CoreCerradura/CoreCerradura/CoreCerradura.h
+++ CoreCerradura/CoreCerradura/CoreCerradura.h
@@ -6,14 +6,11 @@
 //  Copyright (c) 2015 ColemanCDA. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for CoreCerradura.
 FOUNDATION_EXPORT double CoreCerraduraVersionNumber;
 
 //! Project version string for CoreCerradura.
 FOUNDATION_EXPORT const unsigned char CoreCerraduraVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <CoreCerradura/PublicHeader.h>
-
 
