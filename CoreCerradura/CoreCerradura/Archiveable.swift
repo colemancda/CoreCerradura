ee90b0fe9b8f86dec1921c8e01f28fd373584932
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-04-28 15:53:06 -0500

    added documentation for Archiveable

--- CoreCerradura/CoreCerradura/Archiveable.swift
+++ CoreCerradura/CoreCerradura/Archiveable.swift
@@ -6,7 +6,9 @@
 //  Copyright (c) 2015 ColemanCDA. All rights reserved.
 //
 
+/** Protocol that defines whether an entity can be archived or not. Archived entities are basically deleted, but still stored for historical purposes. */
 public protocol Archiveable {
     
+    /* Whether this entity is archived or not. Archived entities are basically deleted, but still stored for historical purposes. */
     var archived: NSNumber { get set }
 }
