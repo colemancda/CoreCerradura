4cca67332a73725c97de0132e4e034b6f525ed40
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-05-04 22:02:47 -0500

    added IDZSwiftCommonCrypto

--- CoreCerradura/CoreCerradura/Authentication.swift
+++ CoreCerradura/CoreCerradura/Authentication.swift
@@ -7,8 +7,7 @@
 //
 
 import Foundation
-import CommonCrypto
-import Crypto
+import IDZSwiftCommonCrypto
 
 // MARK: - Functions
 
@@ -16,36 +15,32 @@ import Crypto
 ///
 /// :param: identifier The identifier (resource ID or username) of the entity trying to authenticate.
 /// :param: secret The secret (e.g. password) of the entity trying to authenticate.
-/// :param: request The URL request being made to the server.
+/// :param: request The URL request being made to the server. Request must include 'Date' and 'Content-MD5' headers
 public func GenerateAuthorizationHeader(identifier: String, secret: String, request: NSURLRequest) -> String {
     
     // Modeled after AWS http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html#UsingTemporarySecurityCredentials
     
-    let contentMD5 = request.HTTPBody?.MD5
+    let contentMD5 = request.valueForHTTPHeaderField("Content-MD5") ?? ""
     
-    var contentMD5String: String = ""
+    let dateString = request.valueForHTTPHeaderField("Date")!
     
-    if contentMD5 != nil {
-        
-        contentMD5String = NSString(data: contentMD5!, encoding: NSUTF8StringEncoding) as! String
-    }
+    let stringToSign = request.HTTPMethod! + contentMD5 + request.URL!.path! + dateString
     
-    let dateString = AuthenticationDateFormatter.stringFromDate(NSDate())
+    let secretData = (secret as NSString).dataUsingEncoding(NSUTF8StringEncoding)!
     
-    let stringToSign = request.HTTPMethod! + contentMD5String + request.URL!.path! + dateString
+    let stringToSignData = (stringToSign as NSString).dataUsingEncoding(NSUTF8StringEncoding)!
     
-    return ""
+    let signedKeyBytes = HMAC(algorithm: HMAC.Algorithm.SHA512, key: secretData).update(secret)!.final()
+    
+    let signedKey = NSData(bytes: signedKeyBytes, length: signedKeyBytes.count)
+    
+    let base64Signature = signedKey.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.allZeros)
+    
+    let jsonAuthorizationObject = [identifier: base64Signature]
+    
+    let jsonData = NSJSONSerialization.dataWithJSONObject(jsonAuthorizationObject, options: NSJSONWritingOptions.allZeros, error: nil)!
+    
+    let authorizationString = NSString(data: jsonData, encoding: NSUTF8StringEncoding)
+    
+    return authorizationString as! String
 }
-
-// MARK: - Private Contants
-
-private let AuthenticationDateFormatter: NSDateFormatter = {
-   
-    let dateFormatter = NSDateFormatter()
-    
-    dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
-    
-    dateFormatter.timeStyle = NSDateFormatterStyle.MediumStyle
-    
-    return dateFormatter
-}()
