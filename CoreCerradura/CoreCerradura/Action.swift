f33c0b234135f674d17e0026501122ac4af7b3f7
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-05-28 13:18:55 -0500

    added 'status' attribute to Action

--- CoreCerradura/CoreCerradura/Action.swift
+++ CoreCerradura/CoreCerradura/Action.swift
@@ -9,49 +9,67 @@
 import Foundation
 import CoreData
 
-/* Encapsulates an action. Used for activity history. */
+/** Encapsulates an action. Used for activity history. */
 public class Action: NSManagedObject {
     
     // MARK: - Properties
     
     // MARK: Attributes
 
-    /* Date this action ocurred. */
+    /** Date this action ocurred. */
     @NSManaged public var date: NSDate
     
-    /* Type of action. Raw value of ActionType. */
+    /** Type of action. Raw value of ActionType. */
     @NSManaged public var type: String
     
     // MARK: Relationships
     
-    /* The lock associated with this action. */
+    /** The lock associated with this action. */
     @NSManaged public var lock: Lock?
     
-    /* The user associated with this action. */
+    /** The user associated with this action. */
     @NSManaged public var user: User?
     
-    /* The permission associated with this action. */
+    /** The permission associated with this action. */
     @NSManaged public var permission: Permission?
+    
+    /** Status the action has been completed. */
+    @NSManaged public var status: String
     
     // MARK: - Initialization
     
     public override func awakeFromInsert() {
         
         self.date = NSDate()
+        
+        self.status = ActionStatus.Completed.rawValue
     }
 }
 
 // MARK: - Enumerations
 
-/* Specifies the type of action. */
+/** Specifies the type of action. */
 public enum ActionType: String {
     
-    /* A new entity was created. */
+    /** A new entity was created. */
     case New = "new"
     
-    /* An entity has been archived / invalidated. */
+    /** An entity has been archived / invalidated. */
     case Archived = "archived"
     
-    /* A lock was unlocked. */
-    case Unlocked = "unlocked"
+    /** A lock was unlocked. */
+    case Unlock = "unlock"
+}
+
+/** Specifies the status of the action. */
+public enum ActionStatus: String {
+    
+    /** Action completed. Default value. */
+    case Completed = "completed"
+    
+    /** Action is pending completion. */
+    case Pending = "pending"
+    
+    /** Action expired. */
+    case Expired = "expired"
 }
